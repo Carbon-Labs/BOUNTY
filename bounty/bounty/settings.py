ea88104bcf463383e013df6e2b58a29ea26c2f32
amexperts <am.experts.online@gmail.com>
2018-05-14 07:22:01 +0200

    added bootstrap4 and smart_selects

--- bounty/bounty/settings.py
+++ bounty/bounty/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'smart_selects',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
